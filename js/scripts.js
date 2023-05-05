c48b99cdd58826861ace8a4049b4f6de19bade14
Elivelton Alves <eliveltonsa92@gmail.com>
2023-05-05 17:45:11 -0300

    adição de tema dark

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,27 @@
+const changeTheme = document.querySelector("#chk");
+const html = document.querySelector("html");
 const hamburgerMenu = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".navbar-menu");
 const navLinks = document.querySelectorAll(".navbar-link");
+
+changeTheme.addEventListener("change", () => {
+  html.classList.toggle("dark");
+
+  if (html.classList.contains("dark")) {
+    localStorage.setItem("dark", 1);
+  } else {
+    localStorage.removeItem("dark", 1);
+  }
+});
+
+const loadTheme = () => {
+  const darkTheme = localStorage.getItem("dark", 1);
+  if (darkTheme) {
+    html.classList.add("dark");
+  }
+};
+
+loadTheme();
 
 hamburgerMenu.addEventListener("click", () => {
   hamburgerMenu.classList.toggle("active");
