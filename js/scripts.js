c10661d7d035461b479c1723b3d8f5dcfc112dc6
Elivelton Alves <eliveltonsa92@gmail.com>
2023-05-11 23:51:55 -0300

    alteração no js e no input de checkbox

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,19 @@
-const changeTheme = document.querySelector("#chk");
 const html = document.querySelector("html");
+const navbarLogo = document.querySelector("#navbar-logo");
+const changeTheme = document.querySelector("#chk");
 const hamburgerMenu = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".navbar-menu");
 const navLinks = document.querySelectorAll(".navbar-link");
+
+const loadTheme = () => {
+  const darkTheme = localStorage.getItem("dark");
+  if (darkTheme) {
+    html.classList.add("dark");
+    changeTheme.checked = true;
+  }
+};
+
+loadTheme();
 
 changeTheme.addEventListener("change", () => {
   html.classList.toggle("dark");
@@ -14,18 +25,14 @@ changeTheme.addEventListener("change", () => {
   }
 });
 
-const loadTheme = () => {
-  const darkTheme = localStorage.getItem("dark", 1);
-  if (darkTheme) {
-    html.classList.add("dark");
-  }
-};
-
-loadTheme();
-
 hamburgerMenu.addEventListener("click", () => {
   hamburgerMenu.classList.toggle("active");
   navMenu.classList.toggle("active");
+});
+
+navbarLogo.addEventListener("click", () => {
+  hamburgerMenu.classList.remove("active");
+  navMenu.classList.remove("active");
 });
 
 navLinks.forEach((link) => {
